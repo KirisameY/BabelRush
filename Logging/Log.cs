27454f353e97896d961159549582ada16b7206da
霧雨烨 <Web_13234@outlook.com>
2024-07-04 02:15:24 +0800

    Log系统雏形

---
+++ Logging/Log.cs
@@ -0,0 +1,12 @@
+﻿namespace BabelRush.Logging;
+
+public record class Log(LogLevel Level, string Message);
+
+public enum LogLevel
+{
+    Debug,
+    Information,
+    Warning,
+    Error,
+    Fatal,
+}
---
+++ Logging/LogManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+
+namespace BabelRush.Logging;
+
+public static class LogManager
+{
+    private static readonly ConcurrentQueue<Log> LogQueue = [];
+    private static readonly LogWriter Writer = new();
+
+    public static void Log(Log log)
+    {
+        LogQueue.Enqueue(log);
+        //Todo: 通知记录器记录日志
+    }
+
+    private class LogWriter
+    {
+        //Todo: 实现异步储存日志
+    }
+}
