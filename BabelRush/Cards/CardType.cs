955fb283b072c8cc88bdbf778d52db425994a034
霧雨烨 <Web_13234@outlook.com>
2024-07-07 19:56:49 +0800

    CardType的注册

--- BabelRush/Cards/CardType.cs
+++ BabelRush/Cards/CardType.cs
@@ -6,18 +6,22 @@ using Godot;
 
 namespace BabelRush.Cards;
 
-public class CardType(string name, Texture2D icon, bool usable, int cost)
+public class CardType(string id, bool usable, int cost, IReadOnlyList<IAction> actions)
     : ICardType
 {
-    public string Name { get; } = name;
-    public Texture2D Icon { get; } = icon;
+    public string Id { get; } = id;
+    public string Name => Registers.CardName.GetItem(Id);
+    public Texture2D Icon => Registers.CardIcon.GetItem(Id);
     public bool Usable { get; } = usable;
     public int Cost { get; } = cost;
-    public IReadOnlyList<IAction> Actions { get; } = [];
+    public IReadOnlyList<IAction> Actions { get; } = actions;
 
     public ICard NewInstance()
     {
         //暂且考虑将有必要用特殊卡牌类的条件判断写在这儿
         return new CommonCard(this);
     }
+
+
+    public static CardType Default { get; } = new CardType("default", false, 0, []);
 }
--- BabelRush/Registers.cs
+++ BabelRush/Registers.cs
@@ -1,12 +1,20 @@
 using BabelRush.Actions;
 using BabelRush.Cards;
 
+using Godot;
+
+using KirisameLib.I18n;
 using KirisameLib.Register;
 
 namespace BabelRush;
 
 public static class Registers
 {
-    private static IRegister<IAction>? Actions { get; set; }
-    private static IRegister<ICardType>? Cards { get; set; }
+    //Card
+    public static IRegister<string> CardName { get; set; } =
+        new I18nRegister<string>(nameof(CardName), id => id);
+    public static IRegister<Texture2D> CardIcon { get; set; } =
+        new I18nRegister<Texture2D>(nameof(CardIcon), _ => new PlaceholderTexture2D());
+    public static IRegister<ICardType> Cards { get; set; } =
+        new CommonRegister<ICardType>(nameof(Cards), _ => CardType.Default);
 }
