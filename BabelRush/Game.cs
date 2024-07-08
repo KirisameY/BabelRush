db2cfb1cf7430434be8dc0c6146210ab6d9ca71b
霧雨烨 <Web_13234@outlook.com>
2024-07-08 15:05:43 +0800

    解决Game里的Null问题

--- BabelRush/Game.cs
+++ BabelRush/Game.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Godot;
 
 using KirisameLib.Logging;
@@ -6,11 +8,29 @@ namespace BabelRush;
 
 public partial class Game : Node
 {
-    #pragma warning disable CS8618
     //Singleton
-    public static Game Instance { get; private set; }
+    private static Game? _instance;
+    public static Game Instance
+    {
+        get => _instance ?? throw new GameNotInitializedException();
+        private set => _instance = value;
+    }
 
-    private Game() => Instance = this;
+    private Game()
+    {
+        Instance = this;
+
+        //Init logger
+    #if DEBUG
+        const LogLevel minWriteLevel = LogLevel.Debug;
+        const LogLevel minPrintLevel = LogLevel.Debug;
+    #else
+        const LogLevel minWriteLevel = LogLevel.Info;
+        const LogLevel minPrintLevel = LogLevel.Warning;
+    #endif
+        LogManager.Initialize(minWriteLevel, minPrintLevel, GD.Print, Project.LogDirPath, Project.Name, Project.MaxLogFileCount);
+        Logger = LogManager.GetLogger("Root");
+    }
 
 
     //Handle notification here
@@ -27,17 +47,6 @@ public partial class Game : Node
     //On Game Start
     public override void _Ready()
     {
-        //Init logger
-    #if DEBUG
-        var minWriteLevel = LogLevel.Debug;
-        var minPrintLevel = LogLevel.Debug;
-    #else
-        var minWriteLevel = LogLevel.Info;
-        var minPrintLevel = LogLevel.Warning;
-    #endif
-        LogManager.Initialize(minWriteLevel, minPrintLevel, GD.Print, Project.LogDirPath, Project.Name, Project.MaxLogFileCount);
-        Logger = LogManager.GetLogger("Root");
-
         Logger.Log(LogLevel.Debug, "Initializing", "Debug On");
         Logger.Log(LogLevel.Info,  "Initializing", "Game Start!");
     }
@@ -57,4 +66,7 @@ public partial class Game : Node
 
     //members
     private Logger Logger { get; set; }
+
+    //Exceptions
+    public class GameNotInitializedException : Exception;
 }
