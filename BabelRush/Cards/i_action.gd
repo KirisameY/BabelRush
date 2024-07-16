0d0daa467c31a675bb7696dab900b11514bf6971
霧雨烨 <Web_13234@outlook.com>
2024-07-16 14:55:00 +0800

    CardInterface完成，但未适配

---
+++ BabelRush/Cards/i_action.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var Icon = $Icon
+@onready var N0 = $N0
+@onready var N1 = $N1
+
+func SetIcon(icon:Texture2D) -> void:
+	Icon.texture = icon
+
+func SetValue(value:int) -> void:
+	if(value<0):
+		N1.visible = false
+		N0.visible = false
+		Icon.position.x = 4
+	elif(value<10):
+		N1.visible = false
+		N0.visible = true
+		N0.position.x = 12.5
+		Icon.position.x = 0
+		N0.frame = value
+	else:
+		N1.visible = true
+		N0.visible = true
+		N0.position.x = 14.5
+		Icon.position.x = 0
+		N0.frame = value%10
+		N1.frame = (value/10)%10
---
+++ BabelRush/Cards/i_feature.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+const n:int = 4
+
+var Features: Array[Sprite2D]
+var Icons: Array[TextureRect]
+
+func _ready():
+	Features.push_back(self)
+	for i in range(1,n):
+		Features.push_back(get_node("Feature%d"%i))
+	for i in range(n):
+		Icons.push_back(get_node("Icon%d"%i))
+
+func SetCount(count:int) -> void:
+	for i in range(Features.size()):
+		Features[i].visible = count>i
+		Icons[i].visible = count>i
+
+func SetIcon(index:int, icon:Texture2D) -> void:
+	if index in range(Icons.size()):
+		Icons[index].texture = icon
