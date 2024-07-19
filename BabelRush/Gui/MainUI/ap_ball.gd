ea115e63910cdc61be37163d37b9e974198139ba
霧雨烨 <Web_13234@outlook.com>
2024-07-19 22:12:44 +0800

    Ap球初步实现

---
+++ BabelRush/Gui/MainUI/ap_ball.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+@onready var Points = get_children()
+
+func SetValue(value:int):
+	for i in range(6):
+		Points[i].visible = i<value
+	if value in range(19):
+		$Num.frame = value
+	else:
+		$Num.frame = 19
---
+++ BabelRush/Tests/UI/Mainscene/MainSceneUiTest.cs
@@ -0,0 +1,34 @@
+using BabelRush.Cards;
+using BabelRush.Gui.MainUI;
+
+using Godot;
+
+namespace BabelRush.Tests.UI.Mainscene;
+
+public partial class MainSceneUiTest : Control
+{
+    private CardField? _cardField;
+    private CardField CardField => _cardField ??= GetNode<CardField>("MainUi/CardField");
+
+    private static StringName StrNameSetValue { get; } = "SetValue";
+
+    public override void _Ready()
+    {
+        // ReSharper disable once StringLiteralTypo
+        Node apBall = GetNode("MainUi/ApBar/Apball");
+        GetNode<HSlider>("HSlider").ValueChanged +=
+            value => apBall.CallDeferred(StrNameSetValue,value);
+    }
+
+    public void Test()
+    {
+        //Test
+        void AddTestCard() => CardField.AddCard(CommonCard.Default);
+        // var tween = CreateTween();
+        // for (int i = 0; i < 8; i++)
+        // {
+        //     tween.TweenCallback(Callable.From(AddTestCard)).SetDelay(0.1);
+        // }
+        AddTestCard();
+    }
+}
