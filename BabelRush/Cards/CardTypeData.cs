6e4ea582366f22dc31ec6a04794c7433a8a73d5e
霧雨烨 <Web_13234@outlook.com>
2024-08-13 19:18:53 +0800

    从toml读入CardType数据

---
+++ BabelRush/Cards/CardTypeData.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using KirisameLib.Extensions;
+
+using BabelRush.Registers;
+
+using Godot;
+
+using Tomlyn.Model;
+
+namespace BabelRush.Cards;
+
+public record CardTypeData(string Id, bool Usable, int Cost, IEnumerable<string> Actions, IEnumerable<string> Features)
+{
+    public CardType ToCardType()
+    {
+        var actions = Actions.Select(id => ActionRegisters.Actions.GetItem(id));
+        var features = Features.Select(id => CardFeatureRegisters.Features.GetItem(id));
+        return new CommonCardType(Id, Usable, Cost, actions, features);
+    }
+
+    public static IEnumerable<CardTypeData> FromTomlTable(TomlTable table)
+    {
+        if (!table.TryGetValue("cards", out var cards)) yield break;
+        if (cards is not TomlTableArray cardList) yield break;
+        foreach (var cardEntry in cardList)
+        {
+            if (!cardEntry.TryGetValue("id",     out var oId) || oId is not string id) continue;
+            if (!cardEntry.TryGetValue("usable", out var oUsable) || oUsable is not bool usable) continue;
+            if (!cardEntry.TryGetValue("cost",   out var oCost) || oCost is not long cost) continue;
+
+            var actions =
+                (cardEntry.GetOrDefault("actions") as TomlArray)?.Select(x => x!.ToString()!) ?? [];
+            var features =
+                (cardEntry.GetOrDefault("features") as TomlArray)?.Select(x => x!.ToString()!) ?? [];
+
+            yield return new CardTypeData(id, usable, (int)cost, actions, features);
+        }
+    }
+}
--- BabelRush/Tests/Card/CardTest.cs
+++ BabelRush/Tests/Card/CardTest.cs
@@ -2,6 +2,9 @@ using BabelRush.Cards;
 
 using Godot;
 
+using Tomlyn;
+using Tomlyn.Model;
+
 using CardInterface = BabelRush.Gui.Card.CardInterface;
 
 namespace BabelRush.Tests.Card;
@@ -15,10 +18,12 @@ public partial class CardTest : Node2D
 
     public void Test()
     {
-        var cardTypeList = ResourceLoader.Load<CardTypeResourceList>("res://Tests/Card/CardTypeList.tres");
+        var cardTypesFile = FileAccess.Open("res://Tests/Card/CardTypeList.toml", FileAccess.ModeFlags.Read);
+        var cardTypes = CardTypeData.FromTomlTable(Toml.ToModel(cardTypesFile.GetAsText()));
         var n = 0;
-        foreach (var cardType in cardTypeList)
+        foreach (var cardType in cardTypes)
         {
+            GD.Print($"Card.{n}");
             var card = CardInterface.GetInstance(cardType.ToCardType().NewInstance());
             AddChild(card);
             card.Position = new(80 + 64 * n, 200);
