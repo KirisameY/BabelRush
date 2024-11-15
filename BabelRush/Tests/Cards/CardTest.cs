2895ddae571aa0ea4e61c99e9886b471c857f414
霧雨烨 <Web_13234@outlook.com>
2024-11-16 02:24:00 +0800

    补回测试代码

--- BabelRush/Tests/Cards/CardTest.cs
+++ BabelRush/Tests/Cards/CardTest.cs
@@ -1,4 +1,12 @@
+using System.Linq;
+
+using BabelRush.Cards;
+using BabelRush.Gui.Cards;
+
 using Godot;
+
+using Tomlyn;
+using Tomlyn.Model;
 
 namespace BabelRush.Tests.Cards;
 
@@ -11,18 +19,18 @@ public partial class CardTest : Node2D
 
     public void Test()
     {
-        //todo:要重构，这里先注释掉了
-        // var cardTypesFile = FileAccess.Open("res://Tests/Cards/CardTypeList.toml", FileAccess.ModeFlags.Read);
+        var cardTypesFile = FileAccess.Open("res://Tests/Cards/CardTypeList.toml", FileAccess.ModeFlags.Read);
         // var cardTypes = ((TomlTableArray)Toml.ToModel(cardTypesFile.GetAsText())["cards"]).Select(CardTypeModel.FromEntry);
-        // //CardTypeData.FromTomlTable(Toml.ToModel(cardTypesFile.GetAsText()));
-        // var n = 0;
-        // foreach (var result in cardTypes)
-        // {
-        //     GD.Print($"Card.{n}");
-        //     var card = CardInterface.GetInstance(result.Convert().NewInstance());
-        //     AddChild(card);
-        //     card.Position = new(80 + 64 * n, 200);
-        //     n++;
-        // }
+        var cardTypes = CardTypeModel.FromSource(Toml.Parse(cardTypesFile.GetAsText()), out _).Select(m=> m.Convert());
+        //CardTypeData.FromTomlTable(Toml.ToModel(cardTypesFile.GetAsText()));
+        var n = 0;
+        foreach (var cardType in cardTypes)
+        {
+            GD.Print($"Card.{n}");
+            var card = CardInterface.GetInstance(cardType.NewInstance());
+            AddChild(card);
+            card.Position = new(80 + 64 * n, 200);
+            n++;
+        }
     }
 }
