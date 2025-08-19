47759409ecc041852f15db13921be4c04fc92b30
霧雨烨 <Web_13234@outlook.com>
2025-08-19 23:06:44 +0800

    无奈todo，这个分支先搁置

---
+++ BabelRush/Timing/TimeScaledAspect.cs
@@ -0,0 +1,11 @@
+using Metalama.Framework.Aspects;
+
+namespace BabelRush.Timing;
+
+public class TimeScaledAspect : OverrideMethodAspect //todo: 等待.net10所以我就不需要用preview语言版本来使用Metalama了 >_<
+{
+    public override dynamic? OverrideMethod()
+    {
+        throw new System.NotImplementedException();
+    }
+}
